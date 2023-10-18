7d18e191aa42e2a9524a6f69c629fe1b44c3732b
Krepixie <biancalessapena@gmail.com>
2023-10-18 09:40:28 -0300

    add calculator functions

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,11 +1,73 @@
 const imagem = document.getElementById("cachorro");
 const container = document.getElementById("container");
-const botao = document.getElementById("mostrar");
+const mostrar = document.getElementById("mostrar");
 const fechar = document.getElementById("fechar");
 
-botao.addEventListener("click", () => {
-    container.style.display = "block";
-    imagem.style.display = "inline";
-})
+mostrar.addEventListener("click", () => {
+  container.style.display = "block";
+  imagem.style.display = "inline";
+  mostrar.style.marginTop = "0";
+});
 
-fechar.addEventListener("")
+fechar.addEventListener("click", () => {
+  container.style.display = "none";
+  mostrar.style.marginTop = "10rem";
+});
+
+//determina qual vai ser a operação realizada
+
+function calcularDoisNumeros(num1, num2, operacao) {
+  let resposta;
+
+  //com if/else
+
+  /*   if (operacao === "*") {
+    resposta = multiplicar(num1, num2);
+  } else if (operacao === "/") {
+    resposta = dividir(num1, num2);
+  } else if (operacao === "+") {
+    resposta = somar(num1, num2);
+  } else if(operacao === "-"){
+    resposta = subtrair(num1,num2);
+  }
+} */
+
+  //com switch case
+  switch (operacao) {
+    case "*":
+      resposta = multiplicar(num1, num2);
+      break;
+
+    case "/":
+      resposta = dividir(num1, num2);
+      break;
+
+    case "+":
+      resposta = somar(num1, num2);
+      break;
+
+    case "-":
+      resposta = subtrair(num1, num2);
+      break;
+  }
+
+  //operações possíveis
+
+  function multiplicar(num1, num2) {
+    return num1 * num2;
+  }
+
+  function dividir(num1, num2) {
+    return num1 / num2;
+  }
+
+  function somar(num1, num2) {
+    return num1 + num2;
+  }
+
+  function subtrair(num1, num2) {
+    return num1 - num2;
+  }
+
+  document.getElementById("resultado").innerHTML = resposta;
+}
